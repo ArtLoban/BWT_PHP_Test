a983554e8bec4ef71a87f1d73017d7d8dcfaa142
Artem Loban <tymanrock@gmail.com>
2018-04-06 15:42:21 +0300

    Just some test commit

---
+++ config/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+    
+    // Обратная связь
+    'feedback/post' => 'feedback/post',
+    'feedback/list' => 'feedback/list',
+    
+    // Пользователь:
+    'user/register' => 'user/register',
+    'user/login' => 'user/login',
+    'user/logout' => 'user/logout',
+    
+    // Главная страница
+    'index.php' => 'site/index', // actionIndex в SiteController
+    '' => 'site/index', // actionIndex в SiteController
+);
